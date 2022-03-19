a2fc104d5f315397c110dffd912e17792253685b
Likhil <surelikhil@gmail.com>
2022-03-19 22:07:31 +0530

    new program added

---
+++ palindrome or not.py
@@ -0,0 +1,12 @@
+n=int(input("Enter num:"))
+temp=n
+rev=0
+while(n>0):
+    rem=n%10
+    rev=(rev*10)+rem
+    n=n//10
+
+if(temp==rev):
+    print("palindrome")
+else:
+    print("not a palindrome")
